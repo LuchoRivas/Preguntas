69b8416cd54e05dd83141c0432487e27a3cfabed
LuchoRivas <lrivas@binit.com.ar>
2019-03-29 14:45:13 -0300

    Archivo

---
+++ Preguntas/Controllers/ArchivoController.cs
@@ -0,0 +1,79 @@
+﻿using Binit.Infraestructura.Website.Models;
+using Binit.Infraestructura.Website.Models.Dominio.Archivo;
+using Binit.Infraestructura.Website.Models.Dominio.General;
+using Preguntas.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Binit.Infraestructura.Website.Controllers
+{
+    public class ArchivoController : Controller
+    {
+        public ActionResult ObtenerImagenPorId(Guid id)
+        {
+            var db = new ApplicationDbContext();
+            var path = ConfigurationManager.AppSettings["PathArchivos"];
+            var file = new Repositorio<Archivo>(db).Traer(id);
+            var extension = Path.GetExtension(file.Nombre);
+            path += id.ToString().Substring(0, 2) + @"\";
+            path += id.ToString().Substring(2, 1) + @"\";
+            path += id.ToString() + extension;
+
+            var bytes = System.IO.File.ReadAllBytes(path);
+
+            return File(bytes, "image/" + extension);
+        }
+
+        public ActionResult SubirArchivoWysiwyg(HttpPostedFileBase file)
+        {
+
+            var db = new ApplicationDbContext();
+
+            //Obtengo el path del web.config
+            var path = ConfigurationManager.AppSettings["PathArchivos"];
+
+            //Obtengo Extension
+            var extension = Path.GetExtension(file.FileName);
+
+            //Guarda en bd
+
+            var imagen = new Archivo();
+            imagen.Nombre = file.FileName;
+            imagen.IdFileManager = imagen.Id.ToString();
+            imagen.Extension = extension;
+
+            //Genero Id
+            var guid = imagen.IdFileManager;
+
+            //Genero el path donde guardare el archivo y si no existe lo genero
+            path += guid.Substring(0, 2) + @"\";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            path += guid.Substring(2, 1) + @"\";
+            if (!Directory.Exists(path))
+                Directory.CreateDirectory(path);
+            path += guid + extension;
+
+            //Obtengo el byte[] de la File enviada
+            MemoryStream target = new MemoryStream();
+            file.InputStream.CopyTo(target);
+            byte[] bytes = target.ToArray();
+
+            //Guardo en Disco
+            System.IO.File.WriteAllBytes(path, bytes);
+
+            new Repositorio<Archivo>(db).Crear(imagen);
+
+            //Link para obtener imagen
+            var link = ConfigurationManager.AppSettings["Core"] + "/Archivo/ObtenerImagenPorId/" + guid;
+
+            //Retorno link del archivo
+            return Json(new { link = link }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Preguntas/Models/Dominio/Archivo.cs
@@ -0,0 +1,17 @@
+﻿using Binit.Infraestructura.Website.Models.Dominio.General;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Binit.Infraestructura.Website.Models.Dominio.Archivo
+{
+    [Table("Archivo")]
+    public class Archivo : Entidad
+    {
+
+        public string Extension { get; set; }
+        public string IdFileManager { get; set; }
+    }
+}
